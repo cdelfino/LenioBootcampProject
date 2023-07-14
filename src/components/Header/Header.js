2d93a877658f06d8979bc5cde3995676797222e6
Constanza Delfino <conidelfino@gmail.com>
2023-07-13 22:06:51 -0300

    changes to header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Header.module.css";
+import SearchIcon from "../icons/SearchIcon";
+import StarIcon from "../icons/StarIcon";
+
+const Header = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <img
+        src="https://logodownload.org/wp-content/uploads/2017/05/marvel-logo-4.png"
+        alt="Marvel Logo"
+        className={styles.logo}
+      />
+      <div className={styles.separator}></div>
+      <SearchIcon className={styles.svg} />
+      <input type="text" placeholder="Buscar" className={styles.searchBar} />
+      <StarIcon className={styles.svg} />
+      <div className={styles.separator}></div>
+    </header>
+  );
+};
+
+export default Header;
